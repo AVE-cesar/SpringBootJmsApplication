aa96332c49e08a3a38f62f217348b64b40ac1d36
AVE-cesar <AVE-cesar@users.noreply.github.com>
2023-09-28 21:24:11 +0200

    Update BusinessServiceImpl.java

--- src/main/java/com/example/demo/service/BusinessServiceImpl.java
+++ src/main/java/com/example/demo/service/BusinessServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.Consts;
 import com.example.demo.FileUtils;
 import com.example.demo.dao.CountryDAO;
 import com.example.demo.model.Country;
@@ -19,19 +18,21 @@ import com.example.demo.model.Country;
 public class BusinessServiceImpl implements BusinessService {
 
 	private final static Logger logger = LoggerFactory.getLogger(BusinessServiceImpl.class);
-	
+
 	@Autowired
 	CountryDAO countryDAO;
-	
+
 	@Autowired
 	JmsTemplate jmsTemplate;
-	
+
+	@Autowired
+	AsyncJMSPut asyncJMSPut;
+
 	@Override
 	@Transactional
 	public void doLogic(String... args) throws Exception {
 		logger.info("--------------________________________------------");
-		
-		
+
 		logger.info("our logic starts here");
 
 		ClassLoader classLoader = getClass().getClassLoader();
@@ -41,48 +42,48 @@ public class BusinessServiceImpl implements BusinessService {
 			data = FileUtils.readFromInputStream(inputStream);
 			logger.info(data);
 		} catch (IOException e) {
-			logger.warn("",e);
+			logger.warn("", e);
 		}
 
-		
-		
 		if (countryDAO != null) {
 
 			Country andorre = new Country(1000L, "Andorre");
-			
+
 			Country luxembourg = new Country(2000L, "Luxembourg");
-			
+
 			logger.info("résultat de la suppression: {}", countryDAO.deleteCountry(andorre));
 			logger.info("résultat de la suppression: {}", countryDAO.deleteCountry(luxembourg));
-			
+
 			Country countryFound = countryDAO.getCountryById(1L);
 
 			logger.info("le pays doit être les USA: {}", countryFound.getName());
 
 			countryFound = null;
 
-			
 			boolean created = countryDAO.createCountry(andorre);
 
 			countryFound = countryDAO.getCountryById(andorre.getId());
 			logger.info("le pays Andorre ={} doit être créé: {}", andorre.getName(), countryFound.getId());
-	 
-	        // Send a JMS message
-	        logger.info("Sending a JMS message.");
-	        jmsTemplate.convertAndSend(Consts.QUEUE_NAME, "Test");
-	        
-	        if ("SimulateAnError".equalsIgnoreCase(args[0])) {
-	        	throw new Exception("SimulateAnError and rollback ALL");
-	        }
-	        created = countryDAO.createCountry(luxembourg);
+
+			// Send a list of JMS messages
+			// Send a JMS message
+			logger.info("Sending a JMS message.");
+			for (int i = 0; i < 1000; i++) {
+				asyncJMSPut.sendJMS(i);
+			}
+
+			if ("SimulateAnError".equalsIgnoreCase(args[0])) {
+				throw new Exception("SimulateAnError and rollback ALL");
+			}
+			created = countryDAO.createCountry(luxembourg);
 
 			countryFound = countryDAO.getCountryById(luxembourg.getId());
 			logger.info("le pays Luxembourg ={} doit être créé: {}", luxembourg.getName(), countryFound.getId());
-	 
+
 		} else {
 			logger.warn("on ne veut pas en être là");
 		}
-		
+
 	}
 
 }
